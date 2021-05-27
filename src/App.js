d99ba6f1ff87313a5aaf8f7eef4e7c4acf049069
HamoHamoHamo <hamo222@naver.com>
2021-05-27 14:39:47 +0000

    #2.4 Protection with PropTypes

--- src/App.js
+++ src/App.js
@@ -1,28 +1,45 @@
-function Potato({name}){
-  return <h1>I love {name}</h1>;
-}
+import propTypes from "prop-types";
 
 const foodILike = [
   {
     id: 1,
     name: "Kimchi",
+    rating: 4,
   },
   {
     id: 2,
     name: "라면",
+    rating: 4.1,
   },
   {
     id: 3,
     name: "밥",
+
   },
   {
     id: 4,
     name: "김밥",
+    rating: 3.4,
   }
 ]
 
+function Food({name, rating}){
+  return (
+    <div>
+      <h1>I love {name}</h1>
+      <h4>{rating}/5</h4>
+    </div>
+  );
+}
+
+Food.propTypes = {
+  name: propTypes.string.isRequired,
+  rating: propTypes.number,
+};
+
+
 function renderFood(dish) {
-  return <Potato key={dish.id} name={dish.name} />
+  return <Food key={dish.id} name={dish.name} rating={dish.rating} />
 }
 
 function App() {
